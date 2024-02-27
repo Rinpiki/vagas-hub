424d3e5bda3ef4926dafa26f84dfb5bfae9602ae
Rinpiki <rinpikibr@gmail.com>
2024-02-27 14:54:27 -0300

    Primeiro commit

---
+++ src/components/list.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function List() {
+  return <div className="flex flex-col justify-center items-center"></div>;
+}
---
+++ src/components/title.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="flex text-7xl text-white font-bold">Vagas Hub</h1>
+      <p className="text-[#a1a1aa] text-xl font-bold mt-5">
+        Hand-picked themes that you can copy and paste into your apps.
+      </p>
+    </div>
+  );
+}
