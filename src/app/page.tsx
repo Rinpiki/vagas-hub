fee8992079511aed9d1e6beca367be2dda6d477c
Rinpiki <rinpikibr@gmail.com>
2024-04-01 09:17:45 -0300

    feat: add new icon in faveicon and fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import ContainerCards from "@/components/cards/containerCards";
-import Header from "@/components/header";
 import Title from "@/components/title";
 
 export default function Home() {
--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { MouseEvent, ReactNode } from "react";
+import { MouseEvent } from "react";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import dataSites from "../data/data";
 export default function Cards() {
   const [data, setData] = useState([]);
   interface Item {
@@ -12,80 +12,6 @@ export default function Cards() {
     image: string;
     status: boolean;
   }
-  const dataSites = [
-    {
-      name: "Indeed",
-      link: "https://www.indeed.com",
-      image: "/thumb/indeed.png",
-      status: false,
-    },
-    {
-      name: "ProgramaThor",
-      link: "https://www.programathor.com.br/",
-      image: "/thumb/pgthor.png",
-      status: false,
-    },
-    {
-      name: "Solid Jobs",
-      link: "https://vagas.solides.com.br/",
-      image: "/thumb/solids.png",
-      status: false,
-    },
-    {
-      name: "NetVagas",
-      link: "https://www.netvagas.com.br/",
-      image: "/thumb/netvagas.png",
-      status: false,
-    },
-    {
-      name: "GeekHunter",
-      link: "https://www.geekhunter.com.br/",
-      image: "/thumb/geekhunter.png",
-      status: false,
-    },
-    {
-      name: "Catho",
-      link: "https://www.catho.com.br/",
-      image: "/thumb/catho.png",
-      status: false,
-    },
-    {
-      name: "Gupy",
-      link: "https://www.gupy.io/",
-      image: "/thumb/gupy.png",
-      status: false,
-    },
-    {
-      name: "Glassdoor",
-      link: "https://www.glassdoor.com.br/Vaga/index.htm",
-      image: "/thumb/glassdoor.png",
-      status: false,
-    },
-    {
-      name: "Bebee",
-      link: "https://www.bebee.com/",
-      image: "/thumb/bebee.png",
-      status: false,
-    },
-    {
-      name: "InfoJobs",
-      link: "https://www.infojobs.com.br/",
-      image: "/thumb/infojobs.png",
-      status: false,
-    },
-    {
-      name: "bne",
-      link: "https://www.bne.com.br/",
-      image: "/thumb/bne.png",
-      status: false,
-    },
-    {
-      name: "Trabalha Brasil",
-      link: "https://www.trabalhabrasil.com.br/",
-      image: "/thumb/tb.png",
-      status: false,
-    },
-  ];
 
   useEffect(() => {
     const now = new Date().getTime();
