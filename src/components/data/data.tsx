8c9989ac6a53e2cba10a1fabeac4bb851173a1fb
Rinpiki <rinpikibr@gmail.com>
2024-04-10 11:55:41 -0300

    feat: rename Thumb to img

--- src/components/data/data.tsx
+++ src/components/data/data.tsx
@@ -2,25 +2,25 @@ const dataSites = [
   {
     name: "Indeed",
     link: "https://www.indeed.com",
-    image: "/Thumb/ind.png",
+    image: "/img/ind.png",
     status: false,
   },
   {
     name: "ProgramaThor",
     link: "https://www.programathor.com.br/",
-    image: "/Thumb/pgthor.png",
+    image: "/img/pgthor.png",
     status: false,
   },
   {
     name: "Solid Jobs",
     link: "https://vagas.solides.com.br/",
-    image: "/Thumb/solids.png",
+    image: "/img/solids.png",
     status: false,
   },
   {
     name: "NetVagas",
     link: "https://www.netvagas.com.br/",
-    image: "/Thumb/netvagas.png",
+    image: "/img/netvagas.png",
     status: false,
   },
   {
@@ -44,31 +44,31 @@ const dataSites = [
   {
     name: "Glassdoor",
     link: "https://www.glassdoor.com.br/Vaga/index.htm",
-    image: "/Thumb/glassdoor.png",
+    image: "/img/glassdoor.png",
     status: false,
   },
   {
     name: "Bebee",
     link: "https://www.bebee.com/",
-    image: "/Thumb/bebee.png",
+    image: "/img/bebee.png",
     status: false,
   },
   {
     name: "InfoJobs",
     link: "https://www.infojobs.com.br/",
-    image: "/Thumb/infojobs.png",
+    image: "/img/infojobs.png",
     status: false,
   },
   {
     name: "bne",
     link: "https://www.bne.com.br/",
-    image: "Thumb/bne.png",
+    image: "img/bne.png",
     status: false,
   },
   {
     name: "Trabalha Brasil",
     link: "https://www.trabalhabrasil.com.br/",
-    image: "Thumb/tb.png",
+    image: "img/tb.png",
     status: false,
   },
 ];
